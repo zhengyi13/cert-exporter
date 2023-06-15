ca7a3851839e7fa4a29311124c41bcea8c161574
Justin M <zhengyi13@gmail.com>
2023-06-14 20:36:05 -0700

    Implement the basics of an actual exporter.
    Based on
    https://gist.github.com/fl64/a86b3d375deb947fa11099dd374660da, which
    turns out to be far more helpful than reading either
    https://prometheus.io/docs/instrumenting/writing_exporters/ or
    https://medium.com/teamzerolabs/15-steps-to-write-an-application-prometheus-exporter-in-go-9746b4520e26
    
    I may have to go back on my assertion that code is *NOT* in fact
    documentation.
    
    Nah.
    
    It turns out that some documentation can be so pointlessly bad that
    code serves better to illustrate what's necessary :/

--- main.go
+++ main.go
@@ -2,12 +2,55 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/zhengyi13/cert-exporter/prober"
 )
 
+type expirationCollector struct {
+	expirationDate *prometheus.Desc
+}
+
+func newExpirationCollector() *expirationCollector {
+	return &expirationCollector{
+		expirationDate: prometheus.NewDesc("expiration_date", "when this cert will expire", nil, nil),
+	}
+}
+
+func (collector *expirationCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- collector.expirationDate
+}
+
+func (collector *expirationCollector) Collect(ch chan<- prometheus.Metric) {
+
+	// Implement logic here to get the actual metric value
+	// TODO write that here:
+	//
+	//
+	var mValue int64
+
+	// Write the latest value for the metric(s) to the prometheus metric channel
+	m1 := prometheus.MustNewConstMetric(collector.expirationDate, prometheus.GaugeValue, float64(mValue))
+	m1 = prometheus.NewMetricWithTimestamp(time.Now(), m1)
+	ch <- m1
+}
+
 func main() {
 	log.Println("exporter main")
+
+
+	ed := newExpirationCollector()
+	prometheus.MustRegister(ed)
+
+	http.Handle("/metrics", promhttp.Handler())
+
+
+
 	var h prober.HostPort
 	h = "foo"
 	prober.Probe(h)
+	log.Fatal(http.ListenAndServe(":9201", nil))
 }
