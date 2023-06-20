22e19d99b0c3279c2ba5f3643aa82a47f19d9640
Justin M <zhengyi13@gmail.com>
2023-06-20 10:03:03 -0700

    Export struct fields, because I'm a dummy.

--- config/config.go
+++ config/config.go
@@ -9,14 +9,14 @@ import (
 
 // HostPort simply separates out the two needed components for getting a cert into labelled fields.
 type HostPort struct {
-	hostname string `yaml:"hostname"`
-	port     int    `yaml:"port"`
+	Hostname string `yaml:"hostname"`
+	Port     int    `yaml:"port"`
 }
 
 type Config []HostPort
 
 // GetConfig takes a filename, and returns either a functional config, or an error.
-func (c *Config) GetConfig(fn string) (*Config, error) {
+func GetConfig(fn string) (*Config, error) {
 	yamlFile, err := ioutil.ReadFile(fn)
 	if err != nil {
 		return nil, fmt.Errorf("Cannot read config file: %w", err)
--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/zhengyi13/cert-exporter/prober"
+	//	"github.com/zhengyi13/cert-exporter/prober"
+	"github.com/zhengyi13/cert-exporter/config"
 )
 
 type expirationCollector struct {
@@ -24,10 +25,11 @@ func (collector *expirationCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.expirationDate
 }
 
+// TODO(zhengyi) rewrite this to take the list of things to probe, and then loop over them in the body
+// metric {label_per_hostport?}
 func (collector *expirationCollector) Collect(ch chan<- prometheus.Metric) {
 
-	// Implement logic here to get the actual metric value
-	// TODO write that here:
+	// TODO(zhengyi) Implement logic here to get the actual metric value
 	//
 	//
 	var mValue int64
@@ -39,18 +41,23 @@ func (collector *expirationCollector) Collect(ch chan<- prometheus.Metric) {
 }
 
 func main() {
-	log.Println("exporter main")
 
-
+	// TODO(zhengyi) read config
+	myconfigfile := "/home/zhengyi/code/cert-exporter/config.yaml"
+	
+	hosts, err := config.GetConfig(myconfigfile)
+	if err != nil {
+		log.Fatalf("Failed to GetConfig: %v", err)
+	}
+	for _, host := range(*hosts) {
+		log.Printf("Getting cert from %s:%d", host.Hostname, host.Port)
+	}	
+	// TODO(zhengyi) for hp in hostports; do ed.Collect(hp)? Nah, probably
+	// just pass the resulting struct/list-o-struct to
+	// collector.Collect and let *it* loop over the collection
 	ed := newExpirationCollector()
 	prometheus.MustRegister(ed)
-
 	http.Handle("/metrics", promhttp.Handler())
 
-
-
-	var h prober.HostPort
-	h = "foo"
-	prober.Probe(h)
 	log.Fatal(http.ListenAndServe(":9201", nil))
 }
