7d14c02f226cca079d383362cacaeec1035e4923
Justin M <zhengyi13@gmail.com>
2023-07-06 17:53:33 -0700

    Now it actually works (!!!)
    In the process, I've learned that:
    
    1) I need to know my config at Collector creation time; I can't create
    a stub Collector and have it read the config - That way leads to
    panics that I don't understand /shrug
    
    2) Prometheus will take the timestamp (a simple in), *force* me to
    cast it to a float64, and then display it in scientific notation
    
    3) Grafana can and will handle this stupidity, but not before adding
    its own: it assumes timestamps are in milliseconds, so I have to take
    my field/value, multiply it by 1000, and then select a Date type, per
    a whole bunch of threads where people want to do exactly that,
    e.g. https://github.com/grafana/grafana/issues/6297

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -20,33 +19,23 @@ type expirationCollector struct {
 	
 }
 
-func newExpirationCollector() *expirationCollector {
+// newExpirationCollector just gives us our basic collector.
+func newExpirationCollector(c *config.Config) *expirationCollector {
 	return &expirationCollector{
-		expirationDate: prometheus.NewDesc("expiration_date", "when this cert will expire", nil, nil),
+		expirationDate: prometheus.NewDesc("expiration_date",
+			"when this cert will expire in unix timestamp format",
+			[]string{"hostport"}, nil),
+		probeConfig: c,
 	}
 }
 
-func (collector *expirationCollector) LoadConfig(filename string) error {
-	// do stuff to load config into collector.RelevantFields
-	c, err := config.GetConfig(filename)
-	if err != nil {
-		return fmt.Errorf("you still need to implement this %s", "bro")
-	}
-	collector.probeConfig = c
-	return nil
-}
-
+// Describe is required. Not sure ATM why.
 func (collector *expirationCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.expirationDate
 }
 
-// TODO(zhengyi) rewrite this to take the list of things to probe, and then loop over them in the body
-// metric {label_per_hostport?}
+// Collect actually pulls together the metric and associated labels whenever the /metrics URL is hit.
 func (collector *expirationCollector) Collect(ch chan<- prometheus.Metric) {
-
-	// TODO(zhengyi) Implement logic here to get the actual metric value
-	//
-	//
 	for _, hp := range(*collector.probeConfig) {
 		ts, err := prober.Probe(hp)
 		if err != nil {
@@ -54,32 +43,34 @@ func (collector *expirationCollector) Collect(ch chan<- prometheus.Metric) {
 			continue
 		}
 		// Write the latest value for the metric(s) to the prometheus metric channel
-		m := prometheus.MustNewConstMetric(collector.expirationDate, prometheus.GaugeValue, float64(ts))
+		m := prometheus.MustNewConstMetric(
+			collector.expirationDate,
+			prometheus.GaugeValue,
+			float64(ts),
+			hp.String(),
+		)
 		m = prometheus.NewMetricWithTimestamp(time.Now(), m)
 		ch <- m
-
 	}
-	
 }
 
 func main() {
 
 	configFile := flag.String("config", "", "path to config file")
 	flag.Parse()
-	
-	hosts, err := config.GetConfig(*configFile)
+
+	config, err := config.GetConfig(*configFile)
+	if err != nil {
+		log.Fatalf("Unable to load config; bailing out: %v", err)
+	}
+
+	ed := newExpirationCollector(config)
+
 	if err != nil {
 		log.Fatalf("Failed to GetConfig: %v", err)
 	}
-	for _, host := range(*hosts) {
-		log.Printf("Getting cert from %s:%d", host.Hostname, host.Port)
-	}	
-	// TODO(zhengyi) for hp in hostports; do ed.Collect(hp)? Nah, probably
-	// just pass the resulting struct/list-o-struct to
-	// collector.Collect and let *it* loop over the collection
-	ed := newExpirationCollector()
+
 	prometheus.MustRegister(ed)
 	http.Handle("/metrics", promhttp.Handler())
-
 	log.Fatal(http.ListenAndServe(":9201", nil))
 }
--- prober/prober.go
+++ prober/prober.go
@@ -9,20 +9,20 @@ import (
 
 func Probe(hp config.HostPort) (timestamp int64, err error){
 	log.Printf("probing %s", hp)
-	config :=tls.Config{}
-	conn, err := tls.Dial("tcp", hp.String(), &config)
+	conn, err := tls.Dial("tcp", hp.String(), &tls.Config{})
 	// error case; die early
 	if err != nil {
 		return timestamp, fmt.Errorf("dial failure: %w", err)
 	}
 	defer conn.Close()
 	// otherwise, get cert details
-	// TODO(zhengyi) turn this into Promethus metrics
 	log.Printf("client connected to %s", conn.RemoteAddr().String())
 	s := conn.ConnectionState()
 	for _, cert := range s.PeerCertificates {
 		log.Printf("CN: %s", cert.Subject)
 		timestamp = cert.NotAfter.Unix()
+		log.Printf("Expiration: %d; float: %f", timestamp, float64(timestamp))
 	}
+
 	return timestamp, err
 }
